05b85b1e7f4c59bc5d0eeb065e5ad429301e9a78
Johnny Chang <zjps3407@gmail.com>
2021-01-13 11:04:29 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
+const User = require('../../models/user')
 
 // @route GET /users/login
 // @desc login form
@@ -15,5 +17,31 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+// @route POST /users/register
+// @desc register 
+// @access Public
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    const user = await User.findOne({ email })
+    if (user) {
+      console.log('user already exists!')
+      return res.render('register', { name, email, password, confirmPassword })
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+      await User.create({
+        name,
+        email,
+        password: hash
+      })
+      console.log('user created!')
+      return res.redirect('/')
+    }
+  } catch (err) {
+    console.log(err)
+  }
+
+})
 
 module.exports = router
