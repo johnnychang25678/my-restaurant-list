06b98c72e81924fed660bbc05b492944771c8032
Johnny Chang <zjps3407@gmail.com>
2021-01-13 17:29:56 +0800

    feat: add facebook login feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
@@ -30,6 +31,34 @@ module.exports = (app) => {
       }
     })
   )
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      const { name, email } = profile._json
+      const user = await User.findOne({ email })
+      if (user) return done(null, user)
+
+      const randomPassword = Math.random().toString(36).slice(-8)
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(randomPassword, salt)
+      const newUser = await User.create({
+        name,
+        email,
+        password: hash
+      })
+      return done(null, newUser)
+    } catch (err) {
+      done(err)
+    }
+
+  }
+
+  ))
+
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/auth')
+
 
 // import route modules
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
+// import authenticator middleware
+const authenticator = require('../middleware/auth')
 
-router.use('/restaurants', auth, restaurants)
-router.use('/search', auth, search)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/search', authenticator, search)
 router.use('/users', users)
-router.use('/', auth, home)
+router.use('/auth', auth)
+router.use('/', authenticator, home)
 
 module.exports = router
